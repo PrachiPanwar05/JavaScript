8d7f3bc0e191ce1c960f3d6340aa26aca016480c
PRACHI <76863155+PrachiPanwar05@users.noreply.github.com>
2023-09-28 16:46:46 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,9 @@
+
+document.write("<table border = 1px><tr><th>Name</th><th>Designation</th><th>Country</th></tr><tr><td>Govind</td><td>Teacher</td><td>India</td></tr><tr><td>Anaysha</td><td>Software Developer</td><td>USA</td></tr><tr><td>Irish</td><td>Cook</td><td>Australia</td></tr></table>");
+
+
+
+
+
+
+l
