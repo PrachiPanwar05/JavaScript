96bc39d4cbbfffbef3310125d8cb354e6bd2d111
PRACHI <76863155+PrachiPanwar05@users.noreply.github.com>
2024-01-12 00:36:51 +0530

    Update JS_This_Keyword.js

--- JS_This_Keyword.js
+++ JS_This_Keyword.js
@@ -19,10 +19,8 @@ hello = () => {
   document.getElementById("demo").innerHTML += this;
 }
 
-//The window object calls the function:
 window.addEventListener("load", hello);
 
-//A button object calls the function:
 document.getElementById("btn").addEventListener("click", hello);
 </script>
 
